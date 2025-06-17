19aa1303af3c96f7a01ca5b62f5502694d02b06e
ghen <ghenwaabuahmad@gmail.com>
2025-06-18 00:02:51 +0300

    moved files around

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -173,7 +173,7 @@ const Header = () => {
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400"/>
                 <input
                     type="text"
-                    placeholder={t("search")}
+                    placeholder={t("Search Everywhere")}
                     value={searchQuery}
                     onChange={handleSearch}
                     onFocus={() => setShowResults(true)}
