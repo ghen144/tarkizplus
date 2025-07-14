8305a20e2ee410a0ea8e481ddd175e47e4b367c4
ghen <ghenwaabuahmad@gmail.com>
2025-07-14 19:21:44 +0300

    oopsie

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -1,7 +1,7 @@
 // src/components/Logo.jsx
 const Logo = () => (
     <img
-        src="/logo-transparent.png"   {/* root-relative path to /public */}
+        src="/logo-transparent.png"
         alt="TarkizPlus logo"
         className="h-20 w-auto mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
     />
